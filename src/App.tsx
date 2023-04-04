22df61f10c72fa5b982823990ba3426b1d8de8cb
Ethan Salonga <ethansalonga28@gmail.com>
2023-04-03 21:10:50 -0400

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./styles/App.css"
+
+function App() {
+  return (
+    <main>
+      <div className="headerContainer">
+        <h1 className="header">Lists Project</h1>
+      </div>
+      <div className="widthContainer">
+        <section>Content</section>
+      </div>
+    </main>
+  )
+}
+
+export default App
