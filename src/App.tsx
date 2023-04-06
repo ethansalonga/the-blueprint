775f50f93409a3ef6ac0ee94d1ff6f32f14dabeb
Ethan Salonga <ethansalonga28@gmail.com>
2023-04-06 17:05:14 -0400

    Redesign and add header

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import "./styles/App.css"
 function App() {
   return (
     <main>
-      <div className="headerContainer">
-        <h1 className="header">Lists Project</h1>
-      </div>
-      <div className="widthContainer">
-        <section>Content</section>
+      <div className="row">
+        <div className="header__container">
+          <h2 className="header__subtitle">Your standard for living life</h2>
+          <h1 className="header__title">The Blueprint</h1>
+        </div>
       </div>
     </main>
   )
